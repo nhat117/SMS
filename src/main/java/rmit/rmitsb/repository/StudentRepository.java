653deed9a594073d5dc716059d980ca2454776b5
nhat117 <s3878174@rmit.edu.vn>
2022-04-27 06:44:14 +0700

    This is a commit message

--- src/main/java/rmit/rmitsb/repository/StudentRepository.java
+++ src/main/java/rmit/rmitsb/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import rmit.rmitsb.model.Student;
 
 
+
 //To use pagination
 //First you have to create an interface
 //extends Paging and Sorting Repository
